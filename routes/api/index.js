ac59acf49198694118085a092813cb657cd291f6
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-09-03 10:34:36 -0500

    updated to match 18.3

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
 
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use('/pizzas', pizzaRoutes);
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
 
 module.exports = router;
